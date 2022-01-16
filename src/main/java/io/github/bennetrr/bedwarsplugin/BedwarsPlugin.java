41cbd4ab3841ff2ecf81834ff6e4a6e1ac64b9fe
bennetr <76742318+bennetrr@users.noreply.github.com>
2022-01-16 20:56:31 +0100

    Added new start command with the cloud framework

--- src/main/java/io/github/bennetrr/bedwarsplugin/BedwarsPlugin.java
+++ src/main/java/io/github/bennetrr/bedwarsplugin/BedwarsPlugin.java
@@ -1,13 +1,25 @@
 package io.github.bennetrr.bedwarsplugin;
 
+import cloud.commandframework.CommandTree;
+import cloud.commandframework.annotations.AnnotationParser;
+import cloud.commandframework.annotations.CommandDescription;
+import cloud.commandframework.annotations.CommandMethod;
+import cloud.commandframework.arguments.parser.ParserParameters;
+import cloud.commandframework.arguments.parser.StandardParameters;
+import cloud.commandframework.bukkit.BukkitCommandManager;
+import cloud.commandframework.bukkit.CloudBukkitCapabilities;
+import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator;
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.meta.CommandMeta;
+import cloud.commandframework.paper.PaperCommandManager;
 import io.github.bennetrr.bedwarsplugin.definitions.Maps;
-import io.github.bennetrr.bedwarsplugin.exceptions.NotEnoughPlayersException;
-import io.github.bennetrr.bedwarsplugin.exceptions.WrongCommandArgumentsException;
 import io.github.bennetrr.bedwarsplugin.game_elements.*;
 import io.github.bennetrr.bedwarsplugin.handlers.*;
 import io.github.bennetrr.bedwarsplugin.utils.WorldEditStuff;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.*;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -16,10 +28,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.Team;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 
 
 public class BedwarsPlugin extends JavaPlugin {
@@ -29,6 +43,9 @@ public class BedwarsPlugin extends JavaPlugin {
     private Location spectatingTeamSpawnpoint;
     private BPGame game = null;
     private World w;
+
+    private BukkitCommandManager<CommandSender> manager;
+    private AnnotationParser<CommandSender> annotationParser;
 
     public void log(Object msg) {
         getServer().getLogger().info("[BedwarsPlugin] " + msg.toString());
@@ -40,6 +57,37 @@ public class BedwarsPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Commands
+        final Function<CommandTree<CommandSender>, CommandExecutionCoordinator<CommandSender>> executionCoordinatorFunction = AsynchronousCommandExecutionCoordinator.<CommandSender>newBuilder().build();
+
+        // Get the command manager
+        try {
+            this.manager = new PaperCommandManager<>(this, executionCoordinatorFunction, Function.identity(), Function.identity());
+        } catch (Exception e) {
+            log("Failed to initialize the command manager");
+            log("Disabling...");
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Register Brigadier mappings
+        if (this.manager.queryCapability(CloudBukkitCapabilities.BRIGADIER)) {
+            this.manager.registerBrigadier();
+        }
+
+        // Register asynchronous completions
+        if (this.manager.queryCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
+            ((PaperCommandManager<CommandSender>) this.manager).registerAsynchronousCompletions();
+        }
+
+        // Create the annotation parser
+        final Function<ParserParameters, CommandMeta> commandMetaFunction = p -> CommandMeta.simple().with(CommandMeta.DESCRIPTION, p.get(StandardParameters.DESCRIPTION, "No description")).build();
+        this.annotationParser = new AnnotationParser<>(this.manager, CommandSender.class, commandMetaFunction);
+
+
+        // Parse all @CommandMethod-annotated methods
+        this.annotationParser.parse(this);
+
         // Locations and maps
         w = getServer().getWorlds().get(0);
         spawnLoc = new Location(w, 999, 43, 7);
@@ -80,44 +128,56 @@ public class BedwarsPlugin extends JavaPlugin {
         }, 1L, 1L);
     }
 
-    public void startGame(int maxPlayersPerTeam, int maxTeams) throws WrongCommandArgumentsException, NotEnoughPlayersException {
-        reset();
+    @CommandMethod("start")
+    @CommandDescription("Start a new bedwars game")
+    private void startCommand(@NonNull CommandSender sender) {
         getServer().broadcast(Component.text("Starting a game!"));
-        log("maxPlayersPerTeam=" + maxPlayersPerTeam);
-        log("maxTeams=" + maxTeams);
-        log("Clear and copy the map");
-        // Clear the old map and copy the new
+        // Reset and copy the map
+        reset();
         WorldEditStuff.clearMap(mapPasteLoc);
         map.copyMap();
 
-        //# Team creation and assignment
-        // Do some validation on the inputs
-        log("Assigning teams");
-        if (maxPlayersPerTeam <= 0 || maxPlayersPerTeam > 6)
-            throw new WrongCommandArgumentsException("maxPlayersPerTeam has to be between 1 and 6");
-        if (maxTeams <= 0 || maxTeams > 4)
-            throw new WrongCommandArgumentsException("maxTeams has to be between 1 and 4");
+        // Get and check the values
+        int maxPlayersTeam1 = getConfig().getInt("maxplayer.team1");
+        int maxPlayersTeam2 = getConfig().getInt("maxplayer.team2");
+        int maxPlayersTeam3 = getConfig().getInt("maxplayer.team3");
+        int maxPlayersTeam4 = getConfig().getInt("maxplayer.team4");
 
-        // Get a copy of the online players list
+        if (maxPlayersTeam1 < 1 || maxPlayersTeam1 > 6 || maxPlayersTeam2 < 0 || maxPlayersTeam2 > 6 || maxPlayersTeam3 < 0 || maxPlayersTeam3 > 6 || maxPlayersTeam4 < 0 || maxPlayersTeam4 > 6) {
+            getServer().broadcast(Component.text("Failed to start a game").color(NamedTextColor.RED));
+            sender.sendMessage(Component.text("The maximum number of players per team is not set or set incorrectly").color(NamedTextColor.RED));
+            sender.sendMessage(Component.text("Please use the /bedwars config command").color(NamedTextColor.RED));
+            return;
+        }
+
         List<Player> playerList = new ArrayList<>(getServer().getOnlinePlayers());
         int playerCount = playerList.size();
-        if (playerCount <= 0) throw new NotEnoughPlayersException("No players online");
+        if (playerCount <= 0) {
+            getServer().broadcast(Component.text("Failed to start a game").color(NamedTextColor.RED));
+            sender.sendMessage(Component.text("There are no players online").color(NamedTextColor.RED));
+            return;
+        }
 
-        // Do some more validation on the inputs
-        if (maxPlayersPerTeam >= playerCount) maxPlayersPerTeam = 1;
-        if (((maxPlayersPerTeam - 1) * maxTeams) > 3)
-            throw new NotEnoughPlayersException("There would be an empty team");
+        if ((maxPlayersTeam1 + maxPlayersTeam2 + maxPlayersTeam3 + maxPlayersTeam4) != playerCount) {
+            getServer().broadcast(Component.text("Failed to start a game").color(NamedTextColor.RED));
+            sender.sendMessage(Component.text("The maximum number of players per team is set incorrectly").color(NamedTextColor.RED));
+            sender.sendMessage(Component.text("Please use the /bedwars config command").color(NamedTextColor.RED));
+            return;
+        }
+
+        List<Integer> maxPlayersPerTeams = new ArrayList<>();
+        maxPlayersPerTeams.add(maxPlayersTeam1);
+        if (maxPlayersTeam2 > 0) maxPlayersPerTeams.add(maxPlayersTeam2);
+        if (maxPlayersTeam3 > 0) maxPlayersPerTeams.add(maxPlayersTeam3);
+        if (maxPlayersTeam4 > 0) maxPlayersPerTeams.add(maxPlayersTeam4);
 
         List<BPTeam> teams = new ArrayList<>();
 
-        for (int i = 0; i < maxTeams; i++) {
+        for (int i = 0; i < maxPlayersPerTeams.size(); i++) {
             BPTeamTemplate template = map.getTeams().get(i);
             List<Player> players = new ArrayList<>();
 
-            for (int j = 0; j < maxPlayersPerTeam; j++) {
-                if (playerList.isEmpty()) break;
-
-                // Get a random player
+            for (int j = 0; j < maxPlayersPerTeams.get(i); j++) {
                 Random rand = new Random();
                 players.add(playerList.remove(rand.nextInt(playerList.size())));
             }
@@ -126,7 +186,6 @@ public class BedwarsPlugin extends JavaPlugin {
         }
 
         game = new BPGame(map, teams, new BPSpectatingTeam(spectatingTeamSpawnpoint, playerList), this);
-        getServer().broadcast(Component.text("Game started!"));
     }
 
     public void stopGame() {
